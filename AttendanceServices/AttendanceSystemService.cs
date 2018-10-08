f5255a08ac83ece09e031642d7a2557722673f6f
Wahib <wahib1900@gmail.com>
2018-10-08 17:44:17 +0500

    Added Service layer

---
+++ AttendanceServices/AttendanceSystemService.cs
@@ -0,0 +1,101 @@
+﻿using Khss.ServiceContracts;
+using KhssData.DomainModels.Attendance;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace khss.Services
+{
+    public class AttendanceSystemService : IAttendanceSystemService
+    {
+        public void AddAttendance(Attendance newAttendance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddClass(Class newClass)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSection(Section newSection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddStudent(Student newStudent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteAttendance(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteClass(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteStudent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DelteSection(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Attendance> GetAllAttendances()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Class> GetAllClasses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Section> GetAllSection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<StudentClass> GetAllStudentClasses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Attendance GetAttendanceById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Class GetClassById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Section GetSectionById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetStudentById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public StudentClass GetStudentClassById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ServiceContracts/IAttendanceSystemService.cs
@@ -0,0 +1,46 @@
+﻿using KhssData.DomainModels.Attendance;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Khss.ServiceContracts
+{
+   public interface IAttendanceSystemService
+    {
+        #region Service Method For Class Domain        
+        IEnumerable<Class> GetAllClasses();
+        Class GetClassById(int id);
+        void AddClass(Class newClass);
+        int DeleteClass(int id);
+        #endregion
+
+        #region Service Method fo Student Domain
+        IEnumerable<Student> GetAllStudents();
+        Student GetStudentById(int id);
+        void AddStudent(Student newStudent);
+        void DeleteStudent(int id);
+
+        #endregion
+                
+        #region Service Method for Section Domain
+        IEnumerable<Section> GetAllSection();
+        Section GetSectionById(int id);
+        void AddSection(Section newSection);
+        int DelteSection(int id);
+
+        #endregion
+
+        #region Service Method for Attendance Domain
+        IEnumerable<Attendance> GetAllAttendances();
+        Attendance GetAttendanceById(int id);
+        void AddAttendance(Attendance newAttendance);
+        int DeleteAttendance(int id);
+        #endregion
+
+        #region Service Method for StudentClass
+        IEnumerable<StudentClass> GetAllStudentClasses();
+        StudentClass GetStudentClassById(int id);        
+        
+        #endregion
+    }
+}
